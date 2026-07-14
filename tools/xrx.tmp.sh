76f8d5976c1764d94d5437fc1b4bb755f3a2cc52
runxuexin <xinrunxue@huawei.com>
2026-07-14 04:38:33 +0000

    tmp record

--- tools/xrx.tmp.sh
+++ tools/xrx.tmp.sh
@@ -1,6 +1,7 @@
 cd csrc
 rm -rf build output 
-bash build.sh -n bit_residual_pack_k8v4 -c ascend910b
+bash build.sh -n "bit_residual_pack_k8v4;bit_residual_attention_paged_k8v4" -c ascend910b
 cd .. 
 ./csrc/output/CANN-custom_ops--linux.aarch64.run --install-path=/root/x00827378/vllm-ascend/vllm_ascend/_cann_ops_custom 
 python tests/e2e/singlecard/xrx_bit_residual_k8v4_key1_unaligned.py
+python tests/e2e/singlecard/xrx_bit_residual_k8v4_golden.py
